7b355f50d4fbc6c64799049b58a33c6c2df37285
Kushal <kushal.471997@gmail.com>
2025-05-05 14:44:48 +0530

    learning switches

--- lab1/ans_controller.py
+++ lab1/ans_controller.py
@@ -24,6 +24,7 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet, ether_types, ipv4, arp
 
 
 class LearningSwitch(app_manager.RyuApp):
@@ -33,6 +34,7 @@ class LearningSwitch(app_manager.RyuApp):
         super(LearningSwitch, self).__init__(*args, **kwargs)
 
         # Here you can initialize the data structures you want to keep at the controller
+        self.switch_forwarding_table = {}
         
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -65,5 +67,45 @@ class LearningSwitch(app_manager.RyuApp):
         
         msg = ev.msg
         datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
 
         # Your controller implementation should start here
+        dpid = datapath.id
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        
+        dst = eth.dst
+        src = eth.src
+        
+        # Learn MAC address of interface
+        if dpid not in self.switch_forwarding_table:
+            self.switch_forwarding_table[dpid] = {}
+            
+        self.switch_forwarding_table[dpid][src] = in_port
+        
+        if dst in self.switch_forwarding_table[dpid]:
+            out_port = self.switch_forwarding_table[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # Installing flow rule
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
